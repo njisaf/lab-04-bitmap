0974eae203fdc21736a02bfadcd43d3d78d39f1e
Nassir Isaf <njisaf@gmail.com>
2016-09-16 10:38:03 -0700

    refactored to control from index, grayscale transform

--- bitmap-nassarah/index.js
+++ bitmap-nassarah/index.js
@@ -1,23 +1,28 @@
 'use strict';
 
-const fs = require('fs');
-const bitmapRead = require('./lib/bitmap-read');
-const BitmapConstructor = require('./model/bitmap-constructor');
+const bitmapReader = require('./lib/bitmap-read');
+const bitmapConstructor = require('./model/bitmap-constructor');
 const bitmapWriter = require('./lib/bitmap-write');
+const programSelect = process.argv[3];
+const fileSelect = process.argv[2];
 
-
-bitmapRead('bitmap.bmp', function(err, data){
-  // console.log('Hello team!');
-  if (err) console.error('omg error!');
-  // console.log('output data', data);
-  // console.log('colors', data.toString('hex', 46, 50));
-  // var seeBitmap = new BitmapConstructor(data);
-  // fs.writeFile('../assets/newbitmap.bmp', data, function(){
-  //   console.log('successful write?');
-  // });
-  // console.log('colorArray', seeBitmap.colorArray);
-  // console.log('pixelArray', seeBitmap.pixelArray);
-  bitmapWriter(data, function(){
-    console.log('bitmapWriter running?');
+bitmapReader(fileSelect, function(err, buffer){
+  if (err) console.error('bitmapRead error!');
+  bitmapConstructor(buffer, fileSelect, function(err, object){
+    if (err) console.error('BitmapConstructor error!'); //will get error here??
+    bitmapWriter(object, programSelect, function(err, newFilePath){
+      console.log('New file saved as ' + newFilePath + ' in assets.');
+    });
   });
 });
+
+
+
+// console.log('output data', data);
+// console.log('colors', data.toString('hex', 46, 50));
+// var seeBitmap = new BitmapConstructor(data);
+// fs.writeFile('../assets/newbitmap.bmp', data, function(){
+//   console.log('successful write?');
+// });
+// console.log('colorArray', seeBitmap.colorArray);
+// console.log('pixelArray', seeBitmap.pixelArray);
