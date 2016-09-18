4ab2f27e1e5c15e3d2a80083403f7799d4f4b18c
Nassir Isaf <njisaf@gmail.com>
2016-09-18 14:35:29 -0700

    small test fix

--- bitmap-nassarah/test/bitmap-constructor-error-test.js
+++ bitmap-nassarah/test/bitmap-constructor-error-test.js
@@ -8,9 +8,7 @@ describe('This tests whether bitmap-constructor is outputting a valid bitmap obj
   it('bitmap-constructor should return an object with a property .isBitmap with a value of \'BM\'', function(done){
     bitmapReader('testimage-actuallyjpg.bmp', function(err, buffer){
       bitmapConstructor(buffer, 'testimage-actuallyjpg.bmp', function(err, object){
-        console.log('isbitmap?', object.isBitmap);
         var testThis = object.isBitmap;
-        console.log('testThis', testThis);
         assert.ok(testThis !== 'BM', 'This property should not read BM');
       });
       done();
