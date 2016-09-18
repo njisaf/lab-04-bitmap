c4cf634573ff15f3f23670010317f96925636662
Nassir Isaf <njisaf@gmail.com>
2016-09-18 14:30:02 -0700

    Cleaning up test images

--- bitmap-nassarah/test/bitmap-constructor-error-test.js
+++ bitmap-nassarah/test/bitmap-constructor-error-test.js
@@ -6,8 +6,8 @@ const bitmapConstructor = require('../model/bitmap-constructor');
 
 describe('This tests whether bitmap-constructor is outputting a valid bitmap object', function() {
   it('bitmap-constructor should return an object with a property .isBitmap with a value of \'BM\'', function(done){
-    bitmapReader('actuallyjpg.bmp', function(err, buffer){
-      bitmapConstructor(buffer, 'actuallyjpg.bmp', function(err, object){
+    bitmapReader('testimage-actuallyjpg.bmp', function(err, buffer){
+      bitmapConstructor(buffer, 'testimage-actuallyjpg.bmp', function(err, object){
         console.log('isbitmap?', object.isBitmap);
         var testThis = object.isBitmap;
         console.log('testThis', testThis);
