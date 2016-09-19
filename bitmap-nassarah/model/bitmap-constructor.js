bab222d0091de657b471146d99cda5f00ce62ef9
Nassir Isaf <njisaf@gmail.com>
2016-09-18 18:21:28 -0700

    Simple endianness check added to bitmap constructor

--- bitmap-nassarah/model/bitmap-constructor.js
+++ bitmap-nassarah/model/bitmap-constructor.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// const fs = require('fs');
-// const bitmapRead = require('../lib/bitmapRead');
+const os = require('os');
 
 module.exports = function buildObject(bufferIn, pathName, callback) {
   var bitmapObject = new Bitmap(bufferIn, pathName);
@@ -13,8 +12,19 @@ function Bitmap(buffer, pathName) {
   this.isBitmap = buffer.toString('utf8', 0, 2);
   this.wholeBuffer = buffer;
   this.header = buffer.toString('utf8', 0, 15);
-  this.fileSize = buffer.readUInt32LE(2, 6);
-  this.offset = buffer.readUInt32LE(10, 14);
+  this.endianness = os.endianness();
+  this.fileSize = checkEndian(buffer, 2, 6);
+  this.offset = checkEndian(buffer, 10, 14);
   this.colorArray = buffer.slice(54, this.offset);
   this.pixelArray = buffer.slice(this.offset);
+}
+
+function checkEndian(buffer, start, finish) {
+  if (os.endianness() === 'LE') {
+    return buffer.readUInt32LE(start, finish);
+  } else if (os.endianness() === 'BE') {
+    return buffer.readUInt32BE(start, finish);
+  } else {
+    console.error('Error!! checkEndian is not functioning');
+  }
 }
